c6cfcffc8723d4251fe367d3fba94155ca5e2044
Morin <morinju@hotmail.fr>
2025-02-21 14:27:23 +0100

    Add(index)in views

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,3 +1,10 @@
+
+@extends('app')
+
+@section('content')
+
 @foreach($books as $book)
     <li>{{$book->title}} by {{$book->user->name}} </li>
 @endforeach
+
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+
+@extends('app')
+
+@section('content')
+
+<h1>Hello world</h1>
+
+@endsection
